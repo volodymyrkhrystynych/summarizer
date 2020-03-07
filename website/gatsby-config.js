acad10799ef02a0a32cc807d8936983a1dd6c07f
Volodymyr Khrystynych <v.khryst@outlook.com>
2020-03-06 23:01:24 -0500

    changed gatsby-config to better reflect our application

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Article Summarizer`,
+    description: `Summarizer articles in ordre to get more information in less time.`,
+    author: `@V&V`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `article-summarizer`,
+        short_name: `summarizer`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,

2662b7beb295cff3ab37a55cb9d38561392e6d91
Volodymyr Khrystynych <v.khryst@outlook.com>
2020-03-13 20:28:32 -0400

    added a text field and setup the basic functionality

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -5,24 +5,42 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const [getter, setter] = React.useState("")
+const IndexPage = () => {
+	const [getter, setter] = React.useState({ text: "" })
+	const buttonHandler = event => {
+		event.preventDefault()
+		console.log(getter)
+	}
+	return (
+		<Layout>
+			<SEO title="Home" />
+			<p>
+				This is going to be a website that uses machine learning and natural
+				language processing in order to summarizer articles
+			</p>
+			<Link to="/page-2/">Go to page 2</Link>
+			<form
+				onSubmit={event => {
+					event.preventDefault()
+					console.log(getter.text)
+				}}
+			>
+				<label>
+					Text in:
+					<textarea
+						type="text"
+						name="input"
+						value={getter.text}
+						onChange={event => setter({ text: event.target.value })}
+					/>
+				</label>
+				<button>Click me</button>
+			</form>
+		</Layout>
+	)
+}
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <p>This is going to be a website that uses machine learning and natural language processing in order to summarizer articles</p>
-    <Link to="/page-2/">Go to page 2</Link>
-    <form>
-	<label>
-		Text in:
-		<textarea type="text" name="input" />
-	</label>
-	<button onClick={() }>Click me</button>
-    </form>
-  </Layout>
-)
-
-    // <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-    //   <Image />
-    // </div>
+// <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//   <Image />
+// </div>
 export default IndexPage

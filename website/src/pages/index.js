988c06760b29f53690814ca3fefeeceb3285bd41
Volodymyr Khrystynych <v.khryst@outlook.com>
2020-07-26 20:11:59 -0400

    fixed a few things

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useEffect} from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -10,46 +10,39 @@ import Summary from "../components/summary"
 
 const IndexPage = () => {
 
-	state = {
-		response: '',
-		post: '',
-		responseToPost: '',
-	  };
 
-	const [mystate, setThisState] = React.useState({
-		response: '',
-		post: '',
-		responseToPost: '',
-	  });
+	const [mystate, setThisState] = React.useState('');
 
-	componentDidMount = () => {
-		callApi()
-		  .then(res => setThisState({ response: res.express }))
-		  .catch(err => console.log(err));
-	  }
-
-	callApi = async () => {
-		const response = await fetch('https://azfzpno1fl.execute-api.us-east-2.amazonaws.com/summariesV1');
+	const callApi = async () => {
+		const response = await fetch('https://azfzpno1fl.execute-api.us-east-2.amazonaws.com/summariesV1/summaries',
+			{
+				method: 'GET',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+					}
+				});
 		const body = await response.json();
 		if (response.status !== 200) throw Error(body.message);
 		
 		return body;
 	  };
+	
+	useEffect( () => {
+		console.log('hello');
+		callApi()
+		  .then(res => setThisState(res.express))
+		  .catch(err => console.log(err));
+	  }, []);
 
-
+	console.log(mystate);
 	return (
 		<Layout>
 			<SEO title="Home" />
 				This is going to be a website that uses machine learning and natural
 				language processing in order to summarizer articles.
 			<div>
-			{
-				mystate.response.map((Summary, _) => {
-					return (
-						<Summary summary={summary}/>
-					);
-				})
-			}
+			
 			</div> 			
 		</Layout>
 	)
